2f7828e42a80b07c04af373507e9734f8f5fc978
David Kyalo <davidmkyalo@gmail.com>
2023-03-18 23:13:02 +0300

    updates

--- examples/__main__.py
+++ examples/__main__.py
@@ -1,1 +1,12 @@
+import os
+import sys
+
 import examples
+from zana.util import try_import
+
+print(sys.argv)
+
+path = sys.argv[-1] if len(sys.argv) > 1 else None
+
+if path:
+    try_import(f"examples.{path}.run")()
--- examples/canvas/__init__.py
+++ examples/canvas/__init__.py
@@ -4,35 +4,42 @@ from itertools import repeat
 
 from zana.canvas import operators, trap
 
-attr_str = (
-    lambda f, i=1: f"{' '.join(repeat('', i))}- {f.name:16}: {f.init = !s:6} {f.kw_only = !s:6} {f.inherited = !s:6} {f.default=}"
-)
-for i, op in enumerate(operators.values(), 1):
-    o, l = op.impl, op.lazy_impl
+from . import nodes
+
+__all__ = [
+    "nodes",
+]
+
+
+def run():
+    attr_str = (
+        lambda f, i=1: f"{' '.join(repeat('', i))}- {f.name:16}: {f.init = !s:6} {f.kw_only = !s:6} {f.inherited = !s:6} {f.default=}"
+    )
+    for i, op in enumerate(operators.values(), 1):
+        o, l = op.impl, op.lazy_impl
+        print(
+            f" - {i:3} : {op.name:16} {op.type.name:8} --> '{op.function.__module__.lstrip('_')}.{op.function.__name__}'",
+            f" -> EAGER: {o.__name__:16} {o.lazy = !s:6}",
+            *(
+                f"   - {f.name:16}: {f.init = !s:6} {f.kw_only = !s:6} {f.inherited = !s:6} {f.default=}"
+                for f in o.__attrs_attrs__
+            ),
+            f" -> LAZY: {l and l.__name__ or '':16} {getattr(l, 'lazy', '???') = !s:6}",
+            *(
+                f"   - {f.name:16}: {f.init = !s:6} {f.kw_only = !s:6} {f.inherited = !s:6} {f.default=}"
+                for f in (l and l.__attrs_attrs__ or ())
+            ),
+            sep="\n      ",
+            end="\n\n",
+        )
+
     print(
-        f" - {i:3} : {op.name:16} {op.type.name:8} --> '{op.function.__module__.lstrip('_')}.{op.function.__name__}'",
-        f" -> EAGER: {o.__name__:16} {o.lazy = !s:6}",
-        *(
-            f"   - {f.name:16}: {f.init = !s:6} {f.kw_only = !s:6} {f.inherited = !s:6} {f.default=}"
-            for f in o.__attrs_attrs__
-        ),
-        f" -> LAZY: {l and l.__name__ or '':16} {getattr(l, 'lazy', '???') = !s:6}",
-        *(
-            f"   - {f.name:16}: {f.init = !s:6} {f.kw_only = !s:6} {f.inherited = !s:6} {f.default=}"
-            for f in (l and l.__attrs_attrs__ or ())
-        ),
-        sep="\n      ",
-        end="\n\n",
+        "TRAP",
+        *(f" - {i}: {n} {v}" for i, (n, v) in enumerate(getmembers(trap, isfunction), 1)),
+        sep="\n  ",
     )
 
+    print(f"{sys.getrecursionlimit() = }")
+    sys.setrecursionlimit(999)
 
-print(
-    "TRAP",
-    *(f" - {i}: {n} {v}" for i, (n, v) in enumerate(getmembers(trap, isfunction), 1)),
-    sep="\n  ",
-)
-
-print(f"{sys.getrecursionlimit() = }")
-sys.setrecursionlimit(999)
-
-print(trap().abc + trap().xyz)
+    print(trap().abc + trap().xyz)
